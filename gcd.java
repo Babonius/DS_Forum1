7c722df553b988d8d60d691889053dcc8353d152
Babonius <144646341+Babonius@users.noreply.github.com>
2024-03-26 13:56:21 +0700

    Add files via upload

--- gcd.java
+++ gcd.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+
+//Group 5: Greatest Common Divisor
+
+// made by Almira
 public class gcd {
-    public static int gcd_finder(int a, int b) {
+    public static long gcd_finder(long a, long b) {
         // base case
         if (b == 0) { // if b reaches 0, return the gcd
             return a;
@@ -8,14 +12,21 @@ public class gcd {
         // recursive case
         return gcd_finder(b, a%b); // recursively call to find GCD of both integers with modulo operation
     }
+
+    // main method
     public static void main(String[] args) {
+        // inputting numbers
         Scanner sc = new Scanner(System.in);
         System.out.print("Input first number: ");
-        int a = sc.nextInt();
+        long a = sc.nextLong();
         Scanner ex = new Scanner(System.in);
         System.out.print("Input second number: ");
-        int b = ex.nextInt();
-        int gcd_result = gcd_finder(a,b);
-        System.out.print("GCD of " + a + " and " + b + " is " + gcd_result );
+        long b = ex.nextLong();
+        // calling the method within a variable
+        long gcd_result = gcd_finder(a,b);
+        // printing out results
+        System.out.println("GCD of " + a + " and " + b + " is " + gcd_result );
+        // largest input possible is the same as largest input possible for long
+
     }
 }
